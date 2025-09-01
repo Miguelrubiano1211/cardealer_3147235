d936622d6f04edbb87677299f02d346a106465d3
Miguel Angel <miguelangelrubianovargas@gmail.com>
2025-09-01 12:09:11 -0500

    new model

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,12 @@ import { StudentsModule } from './students/students.module';
 
 import { instructorsModule } from './instructors/instructors.module';
 import { instructorsController } from './instructors/instructors.controller';
+import { CategoriesModule } from './categories/categories.module';
 
 
 
 @Module({
-  imports: [CustomersModule, VendorsModule, StudentsModule , instructorsModule],
+  imports: [CustomersModule, VendorsModule, StudentsModule , instructorsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/categories/categories.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/prisma/prismaService';
+
+@Injectable()
+export class CategoriesService {
+
+  constructor(private repositorio: PrismaService) {}
+
+
+
+
+  create(createCategoryDto: CreateCategoryDto): string {
+    
+    return "Miguel Angel";
+
+
+
+  }
+
+  findAll() {
+    return `This action returns all categories`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} category`;
+  }
+
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return `This action updates a #${id} category`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} category`;
+  }
+}
